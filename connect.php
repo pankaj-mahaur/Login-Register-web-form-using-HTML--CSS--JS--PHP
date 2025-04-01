70f9b29bff3619363f69c2f8f7d5b63ba8fc8c82
pankaj-mahaur <pankajmahaur2003@gmail.com>
2025-04-01 19:06:49 +0530

    updating php

--- connect.php
+++ connect.php
@@ -3,9 +3,10 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="login";
+$db="login01";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
 }
+
 ?>
